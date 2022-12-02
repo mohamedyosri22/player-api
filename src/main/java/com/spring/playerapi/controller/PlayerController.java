b703305c74651d1410e0661a6298aa8d0c0f6b6b
mhmdyouali <mhmd.you@yahoo.com>
2022-12-02 15:25:21 +0200

    Add player &@RequestBody

--- src/main/java/com/spring/playerapi/controller/PlayerController.java
+++ src/main/java/com/spring/playerapi/controller/PlayerController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,6 +44,13 @@ public class PlayerController {
 			throw new PlayerException("id not found !!");
 		}
 		return p;
+	}
+	
+	// http://localhost:8080/playerapi/api/players
+	@PostMapping("/players")
+	public Player addPlayer(@RequestBody Player player) {
+		playerService.addPlayer(player);
+		return player;
 	}
 	
 		
