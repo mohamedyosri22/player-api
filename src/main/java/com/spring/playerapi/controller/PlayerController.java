762b6e67b7bf4dcbf9e2b5c8dc804bb8f5ecb128
mhmdyouali <mhmd.you@yahoo.com>
2022-12-03 15:55:31 +0200

    delete player and finish

--- src/main/java/com/spring/playerapi/controller/PlayerController.java
+++ src/main/java/com/spring/playerapi/controller/PlayerController.java
@@ -3,9 +3,11 @@ package com.spring.playerapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -49,8 +51,24 @@ public class PlayerController {
 	// http://localhost:8080/playerapi/api/players
 	@PostMapping("/players")
 	public Player addPlayer(@RequestBody Player player) {
-		playerService.addPlayer(player);
+		playerService.addPlayer(player); 
 		return player;
+	}
+	
+	// http://localhost:8080/playerapi/api/players
+	@PutMapping("/players")
+	public String editPlayer(@RequestBody Player player) {
+		playerService.addPlayer(player);
+		return "player updated !!";
+	}
+	
+	@DeleteMapping("/players")
+	public Player deletePlayer(@RequestParam int id) {
+		Player p = playerService.removePlayer(id);
+		if(p==null) {
+			throw new PlayerException("id not found");
+		}
+		return p;
 	}
 	
 		
